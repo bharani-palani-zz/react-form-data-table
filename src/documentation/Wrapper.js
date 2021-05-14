d3253e4c2e7a76d65bf31406af81c07abf1e8b70
Bharani Palani <barani.potshot@gmail.com>
2021-05-15 01:54:35 +0530

    dynamic data with dynamic component added

--- src/documentation/Wrapper.js
+++ src/documentation/Wrapper.js
@@ -3,22 +3,34 @@ import { Route, Switch } from "react-router-dom";
 import { withRouter } from "react-router-dom";
 import { menus } from "./menuComponent";
 import DemoDataTable from "./tables/DemoDataTable";
-import sampleData from "../mockData";
 
-class Wrapper extends React.Component {
-  render() {
-    return (
-      <Switch>
-        <Route exact path="/" component={() => <DemoDataTable data={sampleData} />} />
-        {menus.map((menu, i) => {
-          return (
-            <Route key={i} exact path={menu.href} component={() => <DemoDataTable data={sampleData} />} />
-          );
-        })}
-        <Route path="*" component={() => <DemoDataTable data={sampleData} />} />
-      </Switch>
-    );
-  }
-}
+const Wrapper = props => {
+  return (
+    <Switch>
+      {/* <Route
+        exact
+        path="/"
+        component={() => <DemoDataTable data={sampleData} />}
+      /> */}
+      {menus.map((menu, i) => {
+        return (
+          <Route
+            key={i}
+            exact
+            path={menu.href}
+            component={() => (
+              <DemoDataTable
+                aliasHeaders={menu.aliasHeaders}
+                data={menu.data}
+                config={menu.config}
+              />
+            )}
+          />
+        );
+      })}
+      {/* <Route path="*" component={() => <DemoDataTable data={sampleData} />} /> */}
+    </Switch>
+  );
+};
 
 export default withRouter(Wrapper);
--- src/documentation/tables/DemoDataTable.js
+++ src/documentation/tables/DemoDataTable.js
@@ -2,13 +2,30 @@ import React, { useState } from "react";
 import ReactFormDataTable from "../../root/ReactFormDataTable";
 
 const DemoDataTable = props => {
-  const {data} = props;
+  const { data, aliasHeaders, config } = props;
   const [toggle, setToggle] = useState(false);
+  const [width, setWidth] = useState(14);
+  const widthArray = (start, end) => {
+    return Array(end - start + 1)
+      .fill()
+      .map((_, idx) => start + idx);
+  };
   return (
     <>
       <div className="overflowHidden">
         <div className="infoDiv">
-          <h4 className="heading">Simple Data Table</h4>
+          <div className="heading">Simple Data Table</div>
+          <select
+            className="dropDown"
+            onChange={e => setWidth(Number(e.target.value))}
+          >
+            <option value={10}>Select</option>
+            {widthArray(10, 20).map(v => (
+              <option selected={v === width} value={v}>
+                {v}
+              </option>
+            ))}
+          </select>
           <button className="btn" onClick={() => setToggle(!toggle)}>
             Toggle Theme
           </button>
@@ -16,16 +33,18 @@ const DemoDataTable = props => {
         <pre className="code">
           {`
         <ReactFormDataTable
-          data={data}
-          theme={${!toggle ? "dark" : "light"}}
-          cellWidth="18rem"
+          data={}
+          theme={${toggle ? "dark" : "light"}}
+          cellWidth={${width}rem}
         />
         `}
         </pre>
         <ReactFormDataTable
           data={data}
           theme={toggle ? "dark" : "light"}
-          cellWidth="18rem"
+          cellWidth={`${width}rem`}
+          aliasHeaders={aliasHeaders}
+          config={config}
         />
       </div>
     </>
