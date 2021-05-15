344e9631a0a4e3a14d20dc24a0f0f2621f27d778
Bharani Palani <barani.potshot@gmail.com>
2021-05-16 02:47:30 +0530

    Ajax demo init

--- src/documentation/menuComponent.js
+++ src/documentation/menuComponent.js
@@ -57,7 +57,7 @@ const menus = [
       name: s.name,
       gender: s.gender,
       dob: s.dob,
-      salary: s.salary,
+      salary: s.salary
     })),
     config: {
       footer: {
@@ -71,15 +71,134 @@ const menus = [
   },
   {
     href: "/react-form-data-table/noPagination",
-    label: "No Pagination / Search",
+    label: "Disable Pagination & Search",
+    data: sampleData
+      .map(s => ({
+        id: s.id,
+        name: s.name,
+        gender: s.gender,
+        dob: s.dob,
+        salary: s.salary
+      }))
+      .filter((_, i) => i < 20),
+    config: { footer: { pagination: {} } }
+  },
+  {
+    href: "/react-form-data-table/toolTip",
+    label: "Tooltip",
+    data: sampleData.map(s => ({
+      id: s.id,
+      name: s.name,
+      gender: s.gender,
+      dob: s.dob,
+      salary: s.salary
+    })),
+    showTooltipFor: ["name", "salary"]
+  },
+  {
+    href: "/react-form-data-table/footerTotal",
+    label: "Footer Total",
+    data: sampleData.map(s => ({
+      id: s.id,
+      name: s.name,
+      gender: s.gender,
+      dob: s.dob,
+      salary: s.salary
+    })),
+    showTotal: ["salary"],
+    config: {
+      footer: {
+        total: {
+          locale: "en-IN",
+          currency: "USD",
+          maxDecimal: 2
+        },
+        pagination: {
+          currentPage: "first",
+          recordsPerPage: 10,
+          maxPagesToShow: 5
+        }
+      }
+    }
+  },
+  {
+    href: "/react-form-data-table/footerTotalOnCondition",
+    label: "Footer Total on Condition",
+    data: sampleData.map(s => ({
+      id: s.id,
+      name: s.name,
+      gender: s.gender,
+      dob: s.dob,
+      salary: s.salary
+    })),
+    showTotal: [
+      {
+        whichKey: "salary",
+        forKey: "gender",
+        forCondition: "equals", // includes or equals
+        forValue: ["Male", "Female"],
+        showDifference: { indexes: [0, 1], showStability: true }
+      }
+    ],
+    config: {
+      footer: {
+        total: {
+          locale: "en-IN",
+          currency: "",
+          maxDecimal: 2,
+          doubleEntryBalanceStrings: {
+            zero: "Settled",
+            plus: "Ahead",
+            minus: "Bal"
+          }
+        },
+        pagination: {
+          currentPage: "first",
+          recordsPerPage: 10,
+          maxPagesToShow: 5
+        }
+      }
+    }
+  },
+  {
+    href: "/react-form-data-table/ajaxTable",
+    label: "Ajax",
     data: sampleData.map(s => ({
       id: s.id,
       name: s.name,
       gender: s.gender,
       dob: s.dob,
       salary: s.salary,
-    })).filter((_,i) => i < 20),
-    config: { footer: { pagination: {} } }
+      lastLogin: s.lastLogin
+    })).filter((_, i) => i < 5),
+    showTotal: ["salary"],
+    defaultValues: [{ gender: "Male" }],
+    aliasHeaders: ["Id", "Name", "Gender", "DOB", "Salary", "Last login"],
+    rowElements: [
+      "checkbox",
+      "textbox",
+      {
+        radio: {
+          radioList: [
+            { label: "Male", value: "Male", checked: true },
+            { label: "Female", value: "Female", checked: false }
+          ]
+        }
+      },
+      "date",
+      "number",
+      "dateTime"
+    ],
+    apiInstance: {
+      create: {
+        baseURL: "https://example.com",
+        headers: {Authorization: "1234AbCd"}
+      },
+      ajaxApiUrl: "/v1/updateEmployee",
+      payloadKeyName: "postPayload",
+      ajaxType: "put"
+    },
+    onTableUpdate: () => {}
   }
 ];
 
--- src/documentation/tables/DemoDataTable.js
+++ src/documentation/tables/DemoDataTable.js
@@ -2,7 +2,18 @@ import React, { useState } from "react";
 import ReactFormDataTable from "../../root/ReactFormDataTable";
 
 const DemoDataTable = props => {
-  const { name, data, aliasHeaders, config, showHeaders } = props;
+  const {
+    name,
+    data,
+    aliasHeaders,
+    config,
+    showTooltipFor,
+    showTotal,
+    rowElements,
+    defaultValues,
+    apiInstance,
+    onTableUpdate
+  } = props;
   const [toggle, setToggle] = useState(false);
   const [headers, setToggleHeaders] = useState(true);
   const [width, setWidth] = useState(14);
@@ -20,17 +31,23 @@ const DemoDataTable = props => {
             className="dropDown"
             onChange={e => setWidth(Number(e.target.value))}
           >
-            <option value={10}>Cell-width</option>
+            <option value={10}>cellWidth</option>
             {widthArray(10, 20).map(v => (
               <option selected={v === width} value={v}>
                 {v}
               </option>
             ))}
           </select>
-          <button className={`btn ${headers ? "active" : ""}`} onClick={() => setToggleHeaders(!headers)}>
+          <button
+            className={`btn ${!headers ? "active" : ""}`}
+            onClick={() => setToggleHeaders(!headers)}
+          >
             Toggle Headers
           </button>
-          <button className={`btn ${toggle ? "active" : ""}`} onClick={() => setToggle(!toggle)}>
+          <button
+            className={`btn ${toggle ? "active" : ""}`}
+            onClick={() => setToggle(!toggle)}
+          >
             Toggle Theme
           </button>
         </div>
@@ -42,17 +59,65 @@ const DemoDataTable = props => {
               data
             ).substr(-50)}}`}
           </li>
-          <li className="indend"><b className="asterisk">* </b>theme="{`${toggle ? "dark" : "light"}`}"</li>
-          {width && <li className="indend"><b className="asterisk">* </b>cellWidth="{`${width}`}rem"</li>}
+          <li className="indend">
+            <b className="asterisk">* </b>theme="
+            {`${toggle ? "dark" : "light"}`}"
+          </li>
+          {width && (
+            <li className="indend">
+              <b className="asterisk">* </b>cellWidth="{`${width}`}rem"
+            </li>
+          )}
           {aliasHeaders && (
             <li className="indend">
-              <b className="asterisk">* </b>aliasHeaders={`{${JSON.stringify(aliasHeaders)}}`}
+              <b className="asterisk">* </b>aliasHeaders=
+              {`{${JSON.stringify(aliasHeaders)}}`}
+            </li>
+          )}
+          {showTooltipFor && (
+            <li className="indend">
+              <b className="asterisk">* </b>showTooltipFor=
+              {`{${JSON.stringify(showTooltipFor)}}`}
+            </li>
+          )}
+          {showTotal && (
+            <li className="indend">
+              <b className="asterisk">* </b>showTotal=
+              {`{${JSON.stringify(showTotal)}}`}
+            </li>
+          )}
+          {rowElements && (
+            <li className="indend">
+              <b className="asterisk">* </b>rowElements=
+              {`{${JSON.stringify(rowElements)}}`}
+            </li>
+          )}
+          {defaultValues && (
+            <li className="indend">
+              <b className="asterisk">* </b>defaultValues=
+              {`{${JSON.stringify(defaultValues)}}`}
+            </li>
+          )}
+          {apiInstance && (
+            <li className="indend">
+              <b className="asterisk">* </b>apiInstance=
+              {`{${JSON.stringify(apiInstance)}}`}
             </li>
           )}
           {config && (
-            <li className="indend"><b className="asterisk">* </b>config={`{${JSON.stringify(config)}}`}</li>
+            <li className="indend">
+              <b className="asterisk">* </b>config=
+              {`{${JSON.stringify(config)}}`}
+            </li>
           )}
-          <li className="indend"><b className="asterisk">* </b>showHeaders="{`${headers}`}"</li>
+          {typeof onTableUpdate === "function" && (
+            <li className="indend">
+              <b className="asterisk">* </b>onTableUpdate={`{(data) => console.log(data)}`}
+            </li>
+          )}
+          <li className="indend">
+            <b className="asterisk">* </b>showHeaders="{`${headers}`}"
+          </li>
           <li>{`/>`}</li>
         </ol>
         <div className="note">Fields marked in asterisk(*) are optional</div>
@@ -63,6 +128,12 @@ const DemoDataTable = props => {
           aliasHeaders={aliasHeaders}
           config={config}
           showHeaders={headers}
+          showTooltipFor={showTooltipFor}
+          showTotal={showTotal}
+          rowElements={rowElements}
+          defaultValues={defaultValues}
+          apiInstance={apiInstance}
+          onTableUpdate={data => console.log(data)}
         />
       </div>
     </>
